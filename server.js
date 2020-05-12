42872135e2ec29545302c885f7636fa700c3c8b5
RuslanMZD <ruslan.mzd@gmail.com>
2020-05-12 23:42:40 +0800

    three

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
+
 const app = express();
+const server = require('http').Server(app);
+const io =require('socket.io')(server);
 
 
+// const rooms ={
+
+// 'rooms':[],
+// 'messages':['hello'],
+
+// };
 
 
+const rooms = new Map(); // типо объект но улучшенный, псевдомассив , объект которые наследует методы массива
 
-app.get('/users',(req,res)=>{
-    res.send("ОТВЕТИК");
-    console.log("ХУЙ");
+
+app.get('/rooms',(req,res)=>{
+   
+    res.json(rooms);
+
 });
 
-app.listen(4000,()=>{
+io.on("connection", socket=>{
+    console.log("user connected", socket.id);
+
+});
+
+
+server.listen(4000,(err)=>{  // приходит ошибка
+    if(err){   //если есть ошибка  то возвращаем ее в консоли
+        throw Error(err)
+    }
     console.log('SERVER UP listen 4000 PORT')
 
 })
